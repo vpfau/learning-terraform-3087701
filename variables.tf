b0d553228544f93c03a35bb97d053151cbed2d71
Vitaliy Pfau <1421323+vpfau@users.noreply.github.com>
2024-03-21 20:51:43 -0400

    Fix variable error

--- variables.tf
+++ variables.tf
@@ -12,8 +12,8 @@ variable "ami_filter" {
   })
 
   default = {
-    name   = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
-    owners = "979382823631" # Bitnami
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
   }
 }
 
